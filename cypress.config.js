11381e8258f01bcfd4a070b08e0b72041eb7d92b
AdnanVanni <adnanvanni@example.com>
2025-02-04 15:24:15 +0530

    cmt

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   projectId: 'Sample', // Replace with your Cypress Cloud projectId
+  projectId: "a18hfn",
   e2e: {
     baseUrl: 'https://www.google.com',
     specPattern: 'cypress/e2e/**/*.cy.js',

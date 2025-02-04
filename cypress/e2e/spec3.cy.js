bbae873b08b06d6ffffd8c1f79140c7b270104cb
AdnanVanni <adnanvanni@example.com>
2025-02-04 16:09:18 +0530

    final commit

---
+++ cypress/e2e/spec3.cy.js
@@ -0,0 +1,24 @@
+describe('3New  Search Tests', () => {
+    it('ashould load Google homepage', () => {
+      cy.visit('https://www.google.com');
+      cy.title().should('include','Google');
+    });
+  
+    it('bshould display the search input field', () => {
+      cy.visit('https://www.google.com');
+      cy.get('textarea[name="q"]').should('be.visible');
+    });
+  
+    it('cshould display the search input field', () => {
+      cy.visit('https://www.google.com');
+      cy.get('textarea[name="q"]').should('be.visible');
+    });
+    it('dshould display the search input field', () => {
+      cy.visit('https://www.google.com');
+      cy.get('textarea[name="q"]').should('be.visible');
+    });
+    it('eshould display the search input field', () => {
+      cy.visit('https://www.google.com');
+      cy.get('textarea[name="q"]').should('be.visible');
+    });
+  });
